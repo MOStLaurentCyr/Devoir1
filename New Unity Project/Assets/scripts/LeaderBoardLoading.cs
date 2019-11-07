fe40263bc4e4bd9e7a5932a33cf8bdad0d178801
Marko <mcyx03@gmail.com>
2019-11-06 19:38:51 -0500

    added superMarble

---
+++ New Unity Project/Assets/scripts/LeaderBoardLoading.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SQLite;
+
+public class LeaderBoardLoading : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Awake()
+    {
+        try
+        {
+            SQLiteConnection connect = new SQLiteConnection(@"..\StreamingAssets\Leaderboard.db");
+        }
+        catch
+        {
+            UnityEditor.EditorUtility.DisplayDialog("Connection to the Database has failed!", "ok", "damn!");
+        }
+    }
+}
